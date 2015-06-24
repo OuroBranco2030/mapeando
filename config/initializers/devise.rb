e25088f3c73d64ed1fd7ed57b5cc4b497733ed91
Luiz Fonseca <eu@luiz.cc>
2015-06-23 22:40:15 -0300

    Update contact emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'contato@labrio.cc'
+  config.mailer_sender = 'ourobranco2030@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -233,7 +233,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, '430204343811421', '0df21fb0238bd376a82bdf2f3c2b3703', scope: 'email'
+  # config.omniauth :facebook, '430204343811421', '0df21fb0238bd376a82bdf2f3c2b3703', scope: 'email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
